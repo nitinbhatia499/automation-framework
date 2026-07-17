96284294b5a05392cac5c8bdc7790ab1d946b3ae
NitinBhatia007 <nitinbhatia499@gmail.com>
2026-07-17 13:46:53 +0530

    "Add new browser methods: Firefox, Edge, launchBrowser"

--- automation-framework/src/main/java/io/github/nitinbhatia499/framework/browser/BrowserManager.java
+++ automation-framework/src/main/java/io/github/nitinbhatia499/framework/browser/BrowserManager.java
@@ -2,10 +2,50 @@ package io.github.nitinbhatia499.framework.browser;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class BrowserManager {
-    public static WebDriver createChrome() {
-        return new ChromeDriver();
+    private static WebDriver driver;
+    public static WebDriver createChrome()
+    {
+        driver = new ChromeDriver();
+        return driver;
+    }
+    public static WebDriver createFirefox() {
+        driver = new FirefoxDriver();
+        return driver;
+    }
+
+    public static WebDriver createEdge() {
+        driver = new EdgeDriver();
+        return driver;
+    }
+
+    public static WebDriver launchBrowser(String browserType) {
+        switch (browserType.toLowerCase()) {
+            case "chrome":
+                return createChrome();
+            case "firefox":
+                return createFirefox();
+            case "edge":
+                return createEdge();
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browserType);
+        }
+    }
+
+    public static void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
     }
 }
+
+
 
